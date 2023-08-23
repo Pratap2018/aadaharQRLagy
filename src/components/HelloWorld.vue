f9ef3bc0391b7fa6917619fea70b38fe23ec13ff
Pratap Mridha <pratapmridha@gmail.com>
2023-08-23 12:20:37 +0530

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,117 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <video id="videoInp" style="display: none;"></video>
+    <canvas id="outPutCanvas"></canvas>
+    <Button @click="startVideo" id="startVideo">Scan</Button>
+
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import jsqr from 'jsqr'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      imageData: null
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.imageData !== null) {
+        console.log('process');
+         this.processQr(this.imageData)
+      }
+    }, 5000)
+  },
+  methods: {
+
+    async processQr(imageData) {
+      return new Promise((resolve, reject) => {
+          const code = jsqr(imageData.data, imageData.height, imageData.width)
+          this.imageData = null
+          if (code) {
+            console.log(code.data)
+            alert(code.data)
+          
+          }
+          resolve()
+      })
+   
+
+    },
+
+    async displayOnCanvas() {
+      var video = document.getElementById('videoInp');
+
+      var canvas = document.getElementById('outPutCanvas');
+      canvas.width = 1080;
+      canvas.height = 1920;
+      var context = canvas.getContext('2d');
+      //  display video on canvas sequre 200*200
+      context.drawImage(video, 0, 0, canvas.width,canvas.height);
+
+      // draw rectangle on video 
+      context.beginPath();
+      context.rect(canvas.width/2 -300,canvas.height/2 -300, 600, 600);
+      context.lineWidth = 2;
+      context.strokeStyle = 'white';
+      context.stroke();
+
+
+
+      const imageData = context.getImageData(canvas.width/2 -299,canvas.height/2 -299,600,600);
+      // console.log(imageData);
+      // every 5 second process qr code non blocking
+      this.imageData = imageData;
+      requestAnimationFrame(this.displayOnCanvas)
+
+    }
+
+
+    , startVideo() {
+      var video = document.getElementById('videoInp');
+      const $this = this;
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({
+          video: {
+            width: { min: 640, ideal: 1920, max: 1920 },
+            height: { min: 480, ideal: 1080, max: 1080 },
+
+            facingMode: 'environment',
+
+          }
+        }).then(function (stream) {
+          const track = stream.getVideoTracks()[0]
+          const capabilities = track.getCapabilities()
+          if (capabilities.focusMode?.includes('continuous')) {
+            track.applyConstraints({
+              advanced: [{
+                focusMode: 'continuous',
+                exposureMode: 'continuous',
+                whiteBalanceMode: 'continuous',
+                zoom: "1.5"
+              }]
+            })
+          }
+          video.srcObject = stream
+          video.play();
+          $this.displayOnCanvas();
+
+        });
+      }
+    }
+  },
+
+
+
+
+
 }
 </script>
 
@@ -44,14 +120,17 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
